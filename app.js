5108d13e30b39e3b5ac778a3ff4d76bfd399a7e5
Katerina <edrenina2@gmail.com>
2023-11-20 21:21:57 +0200

    +

---
+++ app.js
@@ -0,0 +1,7 @@
+const line1 = prompt("Enter first line");
+const line2 = prompt("Enter second line");
+const line3 = prompt("Enter third line");
+
+const result = line1 + "\n" + line2 + "\n" + line3;
+
+alert (result);
